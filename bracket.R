d2856087414ffff349d50bfc96b8f5dd4668f520
Sabi Horvat <sabolch.horvat@gmail.com>
2021-03-14 15:47:03 -0700

    updated school names to merge the two datasets

--- bracket.R
+++ bracket.R
@@ -1,5 +1,5 @@
 # Created:      3/15/2020, Sabi Horvat
-# Updated:      2021 data added 3/13/2021
+# Updated:      2021 data added 3/14/2021
 # Instructions: Run all of the code first, then run the app
 #
 # Note that a thorough data analysis has not been conducted on the two data sources
@@ -36,6 +36,21 @@ stats <- import1 %>%
 
 import2 <- read_excel('ncaa basketball 2020-21.xlsx', sheet = 'Sheet1') %>%
   mutate(school = tolower(Team)) 
+import2$school <- gsub("washingtonu", "washington", import2$school)
+import2$school <- gsub("norfolkst", "norfolkstate", import2$school)
+import2$school <- gsub("appalachianst", "appalachianstate", import2$school)
+import2$school <- gsub("calsantabarbara", "uc-santabarbara", import2$school)
+import2$school <- gsub("calsantabarb", "uc-santabarbara", import2$school)
+import2$school <- gsub("usc", "southerncalifornia", import2$school)
+import2$school <- gsub("e.washington", "easternwashington", import2$school)
+import2$school <- gsub("vacommonwealth", "virginiacommonwealth", import2$school)
+import2$school <- gsub("loyolachicago", "loyola(il)", import2$school)
+import2$school <- gsub("houstonu", "houston", import2$school)
+import2$school <- gsub("mountstmarys", "mountst.mary's", import2$school)
+import2$school <- gsub("lsu", "louisianastate", import2$school)
+import2$school <- gsub("ncgreensboro", "northcarolina-greensboro", import2$school)
+import2$school <- gsub("byu", "brighamyoung", import2$school)
+
 lookup1 <- import2 %>%
   select(school, Final, Rot, Date) %>%
   mutate(opponent_rot = ifelse(Rot %% 2 == 0, Rot - 1, Rot + 1) ) %>%
@@ -49,8 +64,8 @@ lookup2 <- import2 %>%
 games <- merge(lookup1, lookup2) %>%
   select(game_date, school, Final, opponent_school, opponent_final) %>%
   mutate(win_or_loss = ifelse(Final > opponent_final, 'W', 'L'))
-
 rm(import1, import2, lookup1, lookup2)
+
 
 # matchup
 compare <- function(team1, team2){
@@ -79,10 +94,7 @@ games_last_ten <- function(team) {
     top_n(10,game_date)   # slice(1:10) also works
 }
 
-
 # shiny app
-
-
 shinyApp(
   ui = fluidPage(
     titlePanel("March Madness Shiny!"),
@@ -98,13 +110,13 @@ shinyApp(
     tableOutput("data1"),
     titlePanel("Head to Head Games"),
     tableOutput("data2"),
-    titlePanel("Team1 last 10 games"),
+    titlePanel("Team1 Last 10 Games"),
     tableOutput("data3"),
-    titlePanel("Team2 last 10 games"),
+    titlePanel("Team2 Last 10 Games"),
     tableOutput("data4")
     
   ),
-  
+
   server = function(input, output) {
     output$data1 <- renderTable({
       compare(input$team1, input$team2)
@@ -124,3 +136,4 @@ shinyApp(
     
   }
 )
+
